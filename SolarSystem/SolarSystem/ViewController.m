100a4babbc074c0e6f1676f8b22f29785c66fd2a
hanatang <jtang31@ucsc.edu>
2017-02-20 01:03:33 -0800

    add details

--- SolarSystem/SolarSystem/ViewController.m
+++ SolarSystem/SolarSystem/ViewController.m
@@ -7,23 +7,21 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h"
 #define PI 3.14159265358979323846
 
-@interface ViewController ()
-@property (nonatomic, strong) CALayer *Earth;
-@property (nonatomic, strong) CALayer *Venus;
-@property (nonatomic, strong) CALayer *Mars;
-@property (nonatomic, strong) CALayer *Uranus;
-@property (nonatomic, strong) CALayer *Jupiter;
-@property (nonatomic, strong) CALayer *Saturn;
-@property (nonatomic, strong) CALayer *Mercury;
-@property (nonatomic, strong) CALayer *centerLayer;
+@interface ViewController (){
+    MBProgressHUD *HUD;
+    UILabel *detail;
+    NSTimer *timer;
+}
+
 
 @end
 
 @implementation ViewController
 
--(void) setPlanet:(CALayer *) layer Speed:(int) speed Radius:(int) radius Color:(UIColor*) color Size:(float) size{
+-(void) setPlanet:(CALayer **)planet Speed:(int) speed Radius:(int) radius Color:(UIColor*) color Size:(float) size{
     
     //orbit
     CAShapeLayer* shapeLayer = [CAShapeLayer layer];
@@ -35,17 +33,19 @@
     shapeLayer.fillColor = [UIColor clearColor].CGColor;
     shapeLayer.strokeColor = [UIColor colorWithRed:169.0f/255.0f green:169.0f/255.0f blue:169.0f/255.0f alpha:0.5].CGColor;
     [self.view.layer addSublayer:shapeLayer];
+     [planets addObject:shapeLayer];
     
     //planet
-    layer = [CALayer layer];
+    *planet=[CALayer layer];
     
-    layer.bounds = CGRectMake(0, 0, 5*size, 5*size);
+    (*planet).bounds = CGRectMake(0, 0, 5*size, 5*size);
     
-    layer.cornerRadius=2.5*size;
+    (*planet).cornerRadius=2.5*size;
     
-    layer.backgroundColor = color.CGColor;
+    (*planet).backgroundColor = color.CGColor;
     
-    [self.view.layer addSublayer:layer];
+    [self.view.layer addSublayer:(*planet)];
+   
     
     CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
     
@@ -65,14 +65,10 @@
     
     animationGroup.removedOnCompletion = NO;
     
-    [layer addAnimation:animationGroup forKey:nil];
-    
-   
-
-    
+    [(*planet) addAnimation:animationGroup forKey:nil];
 }
 
-///监听应用退到后台
+/*//监听应用退到后台
 - (void)didEnterBackground
 {
     //记录暂停时间
@@ -99,7 +95,7 @@
     CFTimeInterval timeSincePause = [self.Earth convertTime:CACurrentMediaTime() fromLayer:nil] - pauseTime;
     //设置开始时间
     self.Earth.beginTime = timeSincePause;
-}
+}*/
 
 
 
@@ -110,47 +106,170 @@
     /*
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];*/
+    CALayer *Earth;
+    CALayer *Venus;
+    CALayer *Mars;
+    CALayer *Uranus;
+    CALayer *Jupiter;
+    CALayer *Saturn;
+    CALayer *Mercury;
+    CALayer *Neptune;
+    CALayer *Sun;
+    
+    planets=[[NSMutableArray alloc] init];
+    
+    
+    self.view.backgroundColor=[UIColor colorWithRed:0.0f/255.0f green:0.0f/255.0f blue:0.0f/255.0f alpha:1.0];
+    
+    /*Title*/
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0,50, self.view.frame.size.width,100)];
+    title.text=@"MY LITTLE SOLAR SYSTEM";
+    title.textAlignment=UITextAlignmentCenter;
+    title.textColor=[UIColor lightGrayColor];
+    title.font =  [UIFont boldSystemFontOfSize:20];
+    [self.view addSubview:title];
 
+    /*Mercury*/
+    [self setPlanet:&Mercury Speed:2.6 Radius:35 Color:[UIColor colorWithRed:238.0f/255.0f green:219.0f/255.0f blue:198.0f/255.0f alpha:1.0] Size:1];
     
-    /*Mercury*/ //238,219,198
-    [self setPlanet:self.Mercury Speed:1 Radius:25 Color:[UIColor colorWithRed:238.0f/255.0f green:219.0f/255.0f blue:198.0f/255.0f alpha:1.0] Size:1];
+    /*Venus*/
+    [self setPlanet:&Venus Speed:4.1 Radius:55 Color:[UIColor colorWithRed:255.0f/255.0f green:186.0f/255.0f blue:64.0f/255.0f alpha:1.0] Size:2];
     
-    /*Venus*/ //255,186,64
-    [self setPlanet:self.Venus Speed:2 Radius:50 Color:[UIColor colorWithRed:255.0f/255.0f green:186.0f/255.0f blue:64.0f/255.0f alpha:1.0] Size:2];
+    /*Earth*/
+    [self setPlanet:&Earth Speed:5.6 Radius:75 Color:[UIColor colorWithRed:50.0f/255.0f green:188.0f/255.0f blue:240.0f/255.0f alpha:1.0] Size:1.5];
     
-    /*Earth*/ //50,188,240
-    [self setPlanet:self.Earth Speed:3 Radius:75 Color:[UIColor colorWithRed:50.0f/255.0f green:188.0f/255.0f blue:240.0f/255.0f alpha:1.0] Size:1.5];
+    /*Mars*/
+    [self setPlanet:&Mars Speed:7.1 Radius:95 Color:[UIColor colorWithRed:249.0f/255.0f green:87.0f/255.0f blue:57.0f/255.0f alpha:1.0] Size:2];
     
-    /*Mars*/ //249,87,57
-    [self setPlanet:self.Mars Speed:4 Radius:100 Color:[UIColor colorWithRed:249.0f/255.0f green:87.0f/255.0f blue:57.0f/255.0f alpha:1.0] Size:2];
+    /*Jupiter*/
+    [self setPlanet:&Jupiter Speed:8.6 Radius:115 Color:[UIColor colorWithRed:252.0f/255.0f green:147.0f/255.0f blue:75.0f/255.0f alpha:1.0] Size:3.5];
     
-    /*Jupiter*/ //252,147,75
-    [self setPlanet:self.Jupiter Speed:5 Radius:125 Color:[UIColor colorWithRed:252.0f/255.0f green:147.0f/255.0f blue:75.0f/255.0f alpha:1.0] Size:3.5];
+    /*Saturn*/
+    [self setPlanet:&Saturn Speed:10.1 Radius:135 Color:[UIColor colorWithRed:255.0f/255.0f green:239.0f/255.0f blue:178.0f/255.0f alpha:1.0] Size:3];
     
-    /*Saturn*/ //255,239,178
-    [self setPlanet:self.Saturn Speed:6 Radius:150 Color:[UIColor colorWithRed:255.0f/255.0f green:239.0f/255.0f blue:178.0f/255.0f alpha:1.0] Size:3];
+    /*Uranus*/
+    [self setPlanet:&Uranus Speed:11.6 Radius:155 Color:[UIColor colorWithRed:132.0f/255.0f green:205.0f/255.0f blue:200.0f/255.0f alpha:1.0] Size:1.5];
     
-    /*Uranus*/ //132,205,200
-    [self setPlanet:self.Uranus Speed:7 Radius:175 Color:[UIColor colorWithRed:132.0f/255.0f green:205.0f/255.0f blue:200.0f/255.0f alpha:1.0] Size:1.5];
-    
-
+    /*Neptune*/
+    [self setPlanet:&Neptune Speed:13.1 Radius:175 Color:[UIColor colorWithRed:117.0f/255.0f green:153.0f/255.0f blue:235.0f/255.0f alpha:1.0] Size:1];
     
     /*Sun*/
+    Sun=[CALayer layer];
+    Sun.bounds = CGRectMake(0,0, 30, 30);
+    Sun.cornerRadius=15;
+    Sun.position = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    Sun.backgroundColor = [UIColor colorWithRed:253.0f/255.0f green:239.0f/255.0f blue:137.0f/255.0f alpha:1.0].CGColor;
+    [self.view.layer addSublayer:Sun];
     
-    self.centerLayer = [CALayer layer];
+    /*Note*/
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:@"Mercury● Venus● Earth● Mars●\n Jupiter● Saturn● Uranus● Neptune●"];
     
-    self.centerLayer.bounds = CGRectMake(0,0, 20, 20);
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:238.0f/255.0f green:219.0f/255.0f blue:198.0f/255.0f alpha:1.0] range:NSMakeRange(0,8)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:7] range:NSMakeRange(0,8)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:255.0f/255.0f green:186.0f/255.0f blue:64.0f/255.0f alpha:1.0] range:NSMakeRange(9,6)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:13] range:NSMakeRange(9,6)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:50.0f/255.0f green:188.0f/255.0f blue:240.0f/255.0f alpha:1.0] range:NSMakeRange(16,6)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:10] range:NSMakeRange(16,6)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:249.0f/255.0f green:87.0f/255.0f blue:57.0f/255.0f alpha:1.0] range:NSMakeRange(23,5)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:13] range:NSMakeRange(23,5)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:252.0f/255.0f green:147.0f/255.0f blue:75.0f/255.0f alpha:1.0] range:NSMakeRange(30,8)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:24] range:NSMakeRange(30,8)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:255.0f/255.0f green:239.0f/255.0f blue:178.0f/255.0f alpha:1.0] range:NSMakeRange(39,7)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:21] range:NSMakeRange(39,7)];
+     [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:132.0f/255.0f green:205.0f/255.0f blue:200.0f/255.0f alpha:1.0] range:NSMakeRange(47,7)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:10] range:NSMakeRange(47,7)];
+    [str addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:117.0f/255.0f green:153.0f/255.0f blue:235.0f/255.0f alpha:1.0] range:NSMakeRange(55,8)];
+    [str addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:7] range:NSMakeRange(55,8)];
     
-    self.centerLayer.cornerRadius=10;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(butClick:)];
+    [self.view addGestureRecognizer:tap];
     
-    self.centerLayer.position = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    UILabel *note = [[UILabel alloc] initWithFrame:CGRectMake(0,500, self.view.frame.size.width,200)];
+    note.attributedText = str;
+    note.textAlignment=UITextAlignmentCenter;
+    note.numberOfLines=2;
+    [self.view addSubview:note];
     
-    //253,239,137
-    self.centerLayer.backgroundColor = [UIColor colorWithRed:253.0f/255.0f green:239.0f/255.0f blue:137.0f/255.0f alpha:1.0].CGColor;
-    
-    [self.view.layer addSublayer:self.centerLayer];
+    /*detail*/
+    detail = [[UILabel alloc] initWithFrame:CGRectMake(0,500, self.view.frame.size.width,50)];
+    detail.text=@"show detail";
+    detail.textAlignment=UITextAlignmentCenter;
+    detail.hidden=YES;
+    detail.textColor=[UIColor colorWithRed:169.0f/255.0f green:169.0f/255.0f blue:169.0f/255.0f alpha:0.5];
+    [self.view addSubview:detail];
+}
+
+
+-(void)butClick:(UITapGestureRecognizer *)gesture{
+    CGPoint clickPoint =  [gesture locationInView:self.view];
+    for(int i=0;i!=[planets count];i++)
+    {
+        CAShapeLayer* orbit=[planets objectAtIndex:i];
+        if (CGPathContainsPoint(orbit.path,NULL,clickPoint,YES))
+        {
+           //show description of the selected planet
+            NSLog(@"%d",i);
+            orbit.strokeColor = [UIColor colorWithRed:169.0f/255.0f green:169.0f/255.0f blue:169.0f/255.0f alpha:1.0].CGColor;
+            switch (i) {
+                case 0:
+                    detail.text=@"Mercury";
+                    break;
+                case 1:
+                    detail.text=@"Venus";
+                    break;
+                case 2:
+                    detail.text=@"Earth";
+                    break;
+                case 3:
+                    detail.text=@"Mars";
+                    break;
+                case 4:
+                    detail.text=@"Jupiter";
+                    break;
+                case 5:
+                    detail.text=@"Saturn";
+                    break;
+                case 6:
+                    detail.text=@"Uranus";
+                    break;
+                case 7:
+                    detail.text=@"Neptune";
+                    break;
+                default:
+                    break;
+            }
+            detail.hidden=NO;
+            [timer invalidate];
+            [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:nil];
+            timer=[NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(helper) userInfo:nil repeats:YES];
+            [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+            break;
+        }
+    }
     
 }
+
+-(void) helper{
+    for(int i=0;i!=[planets count];i++)
+    {
+        CAShapeLayer* orbit=[planets objectAtIndex:i];
+        orbit.strokeColor = [UIColor colorWithRed:169.0f/255.0f green:169.0f/255.0f blue:169.0f/255.0f alpha:0.5].CGColor;
+    }
+    detail.hidden=YES;
+}
+
+/*警告框 提示框 设置*/
+- (void)showAlert{
+    HUD = [[MBProgressHUD alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2-10,450,10,20)];
+    [self.view addSubview:HUD];
+    HUD.mode = MBProgressHUDModeText;
+    //设置对话框文字
+    HUD.label.text =@"ddd";
+    [HUD showAnimated:YES];
+    [HUD hideAnimated:YES afterDelay:2];
+}
+
+
 
 
 - (void)didReceiveMemoryWarning {
